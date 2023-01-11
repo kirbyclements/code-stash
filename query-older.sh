265fd5b2013d62e74700da38d3b8e1a845e02103
“kirbyclements” <k.clements@soasecure.com>
2023-01-11 09:07:02 -0600

    ctime to -1

--- query-older.sh
+++ query-older.sh
@@ -17,7 +17,7 @@ echo "<BODY>"
 
 cd /trvapps/dev_datapower_backups
 
-zipfiles=$(ls -lt | find -name "*backup*.zip" -type f -ctime -2)
+zipfiles=$(ls -lt | find -name "*backup*.zip" -type f -ctime -1)
 for zipfile in $zipfiles; do
 	echo "<BR>DataPower Export $zipfile<BR>"
 	echo "<TABLE>"
